05f25b0ea8afb3b51d0d87677dba014b88986fa0
prachiheda <prachiheda@gmail.com>
2023-09-15 14:21:09 -0700

    file structuering

---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+import Hero from './Hero'
+import Nav from './Nav'
+import CustomerReviews from './CustomerReviews'
+import Footer from './Footer'
+import PopularProducts from './PopularProducts'
+import Services from './Services'
+import SpecialOffer from './SpecialOffer'
+import Subscribe from './Subscribe'
+import SuperQuality from './SuperQuality'
+
+export {
+    Hero, 
+    PopularProducts, 
+    Nav, 
+    CustomerReviews, 
+    Footer, 
+    Services, 
+    SpecialOffer, 
+    Subscribe, 
+    SuperQuality
+}
